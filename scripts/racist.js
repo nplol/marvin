d8f4bfaaeb5661c50b818c2658450a75e4c03bd3
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-04-15 13:49:53 +0200

    added marvin's racial tendencies

---
+++ scripts/racist.js
@@ -0,0 +1,18 @@
+// Description:
+//  It's important that we do not descend into madness and racism
+//
+// Dependencies:
+//  None
+//
+// Configuration:
+//  None
+// Commands:
+//  marvin noracist - Receive appropriate gif
+
+module.exports = function (robot) {
+
+  robot.respond(/noracist/, function (msg) {
+    msg.send "http://i.imgur.com/HEOJs.gif" 
+  }
+
+}
